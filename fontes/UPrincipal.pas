cb448398988fb7edb50418c02e1568e6fc6b361a
antonio-abrantes <antonio_wac@hotmail.com>
2021-03-16 20:05:53 -0300

    Commit layout

--- fontes/UPrincipal.pas
+++ fontes/UPrincipal.pas
@@ -4,10 +4,40 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons, Vcl.StdCtrls,
+  System.ImageList, Vcl.ImgList;
 
 type
   TfrmPrincipal = class(TForm)
+    Shape1: TShape;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    Shape2: TShape;
+    Label1: TLabel;
+    Label2: TLabel;
+    Shape3: TShape;
+    Shape4: TShape;
+    Shape5: TShape;
+    Shape6: TShape;
+    Label3: TLabel;
+    Shape7: TShape;
+    Shape8: TShape;
+    Label4: TLabel;
+    Shape9: TShape;
+    Label5: TLabel;
+    Label6: TLabel;
+    SpeedButton4: TSpeedButton;
+    Shape11: TShape;
+    Shape12: TShape;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Shape10: TShape;
+    Label10: TLabel;
+    Label11: TLabel;
+    Shape13: TShape;
+    Label12: TLabel;
   private
     { Private declarations }
   public
